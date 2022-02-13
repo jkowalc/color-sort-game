38c07263df874ec7ec79a2bf285b3c972358d882
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-02-13 10:06:21 +0100

    Implement tests for pour

--- tests/test_ampule.py
+++ tests/test_ampule.py
@@ -1,4 +1,4 @@
-from ampule import Ampule
+from ampule import Ampule, pour
 from color import Color
 
 
@@ -22,3 +22,27 @@ def test_get_top_color_height_2():
     amp = Ampule(colors=colors)
     assert amp.get_top_color() == Color("red")
     assert amp.get_top_color_height() == 2
+
+
+def test_pour():
+    colors1 = [
+        Color("red"),
+        Color("blue"),
+        Color("blue")
+    ]
+    colors2 = [
+        Color("blue"),
+        Color("blue")
+    ]
+    amp1 = Ampule("A", max_height=3, colors=colors1)
+    amp2 = Ampule("B", max_height=3, colors=colors2)
+    pour(amp1, amp2)
+    assert amp1.colors == [
+        Color("red"),
+        Color("blue")
+    ]
+    assert amp2.colors == [
+        Color("blue"),
+        Color("blue"),
+        Color("blue")
+    ]
